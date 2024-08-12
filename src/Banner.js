394288448af5f1186223d568da786d5cc6f684aa
Manirul Islam <154756557+devmanirul@users.noreply.github.com>
2024-08-12 21:41:39 +0600

    updates

--- src/Banner.js
+++ src/Banner.js
@@ -5,16 +5,16 @@ function Banner() {
             <div className="container">
                 {/* left banner */}
             <div className="left">
-                <img src="https://www.police.gov.bd/storage/upload/poster/lBCgBZQO2pttAyv8Kgy3Lz7XvWKVreSsd02L67In.jpg" alt="" className="" />
+                <img src="https://www.police.gov.bd/storage/upload/poster/lBCgBZQO2pttAyv8Kgy3Lz7XvWKVreSsd02L67In.jpg" alt="" className="imgLeft" />
             </div>
             {/* right banner */}
             <div className="right">
                 {/* img */}
                 <img src="https://www.police.gov.bd/storage/upload/file/PSIAgoFSgkTWdSU2RAiia5SsEZRI0IrQ2zcVK63x.png" alt="" className="igpImg" />
                 {/* IGP name */}
-                <h5 className="">Md. Mainul Islam, ndc
+                <h5 className="igpName">Md. Mainul Islam, ndc <br />
                     Inspector General</h5>
-                <a href="#igpmessages" className="">Massages from IGP {">>"}</a>
+                <a href="#igpmessages" className="igpMassege">Massages from IGP {">>"}</a>
             </div>
             </div>
         </div>
