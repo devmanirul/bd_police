496226af2d462d7bff37cc6fbc0fd8a84fa47389
Manirul Islam <154756557+devmanirul@users.noreply.github.com>
2024-08-13 19:01:56 +0600

    updates

--- src/Carojol.js
+++ src/Carojol.js
@@ -5,24 +5,24 @@ function Carojol() {
             <div className="container">
                  {/* img1 */}
                 <div className="CarojolOne">
-                    <div className="slide">
+                    <div className="slide1">
                          <img
                         src="https://www.police.gov.bd/storage/upload/gallery/QBYZ7c6P75XVQ8u2MuhxFKCTXWYqzt9GhBR4nsyU.jpg"
                         alt=""
                         className="IgpImage"
                             />
-                        <p className="igpSpece">নভাগত আইজিপি মহোদয়ের প্রেস বিজ্ঞপ্তি</p> 
+                        <p className="igpSpece">মহামান্য রাষ্ট্রপতির সাথে নবাগত আইজিপি মহোদয়  সাক্ষাৎ করেন।</p> 
                     </div>   
                 </div>
                  {/* img2 */}
                 <div className="CarojolTwo">
-                    <div className="slide">
+                    <div className="slide2">
                          <img
                         src="https://www.police.gov.bd/storage/upload/gallery/kdXeIE3xgKUnP4b4JzJb0PVDSwhPLIit3j3ernoR.jpg"
                         alt=""
                         className="IgpImage"
                             />
-                        <p className="igpSpece">মহামান্য রাষ্ট্রপতির সাথে নবাগত আইজিপি মহোদয়  সাক্ষাৎ করেন।</p> 
+                        <p className="igpAddress">নভাগত আইজিপি মহোদয়ের প্রেস বিজ্ঞপ্তি</p> 
                     </div>   
                 </div>
             </div>
