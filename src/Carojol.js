606568705f21d6fac7fcd27e4a3bc8cf3fb95646
Manirul Islam <154756557+devmanirul@users.noreply.github.com>
2024-08-13 20:42:56 +0600

    update

--- src/Carojol.js
+++ src/Carojol.js
@@ -1,30 +1,32 @@
 import "./Carojol.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 function Carojol() {
     return (
         <div className="Carojol">
             <div className="container">
-                 {/* img1 */}
-                <div className="CarojolOne">
-                    <div className="slide1">
-                         <img
-                        src="https://www.police.gov.bd/storage/upload/gallery/QBYZ7c6P75XVQ8u2MuhxFKCTXWYqzt9GhBR4nsyU.jpg"
-                        alt=""
-                        className="IgpImage"
-                            />
-                        <p className="igpSpece">মহামান্য রাষ্ট্রপতির সাথে নবাগত আইজিপি মহোদয়  সাক্ষাৎ করেন।</p> 
-                    </div>   
-                </div>
-                 {/* img2 */}
-                <div className="CarojolTwo">
-                    <div className="slide2">
-                         <img
-                        src="https://www.police.gov.bd/storage/upload/gallery/kdXeIE3xgKUnP4b4JzJb0PVDSwhPLIit3j3ernoR.jpg"
-                        alt=""
-                        className="IgpImage"
-                            />
-                        <p className="igpAddress">নভাগত আইজিপি মহোদয়ের প্রেস বিজ্ঞপ্তি</p> 
-                    </div>   
-                </div>
+                <Slider {...settings}>
+                    <div className="CarojolOne">
+                        {/* img1 */}
+                        <div className="slide">
+                            <img
+                            src="https://www.police.gov.bd/storage/upload/gallery/QBYZ7c6P75XVQ8u2MuhxFKCTXWYqzt9GhBR4nsyU.jpg"
+                            alt=""
+                            className="IgpImage"
+                                />
+                            <p className="igpSpece">মহামান্য রাষ্ট্রপতির সাথে নবাগত আইজিপি মহোদয়  সাক্ষাৎ করেন।</p> 
+                        </div> 
+                        {/* img2 */}
+                        <div className="slide">
+                            <img
+                            src="https://www.police.gov.bd/storage/upload/gallery/kdXeIE3xgKUnP4b4JzJb0PVDSwhPLIit3j3ernoR.jpg"
+                            alt=""
+                            className="IgpImage"
+                                />
+                            <p className="igpSpece">নভাগত আইজিপি মহোদয়ের প্রেস বিজ্ঞপ্তি</p>
+                        </div>
+                    </div>
+                </Slider>
             </div>
         </div>
     );
