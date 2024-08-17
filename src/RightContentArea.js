712e8271388301ecbf468d97ad3feba645c0665c
Manirul Islam <154756557+devmanirul@users.noreply.github.com>
2024-08-18 00:34:40 +0600

    update right caontant

--- src/RightContentArea.js
+++ src/RightContentArea.js
@@ -1,26 +1,42 @@
 import "./ImportantLinkRight.css";
 import "./RightContentArea.css";
 import ImportantLinkRight from "./ImportantLinkRight";
+import press from "./assets/images/Press.png";
+import Recruitment from "./assets/images/Recruitment.png";
+import procurement from "./assets/images/Procurement.png";
+import legal from "./assets/images/Legal.png";
+import detective from "./assets/images/Detective.png";
+import diabetis from "./assets/images/Diabetis.png";
+import exsm from "./assets/images/Exam.jpeg";
+import Accident from "./assets/images/Accident.jpeg";
 
 const RightContentArea = () => {
-    return ( <div className="right-content-area">
+    return (
+      <div className="right-content-area">
         {/* 1 */}
-<ImportantLinkRight title={"Press Release"} logo={"https://www.police.gov.bd/storage/upload/announcement/btLkgP7ZYixPpI1jfFlYG1qBMVp7qBp57Vh2ovrI.png"}/>
-{/* 2 */}
-<ImportantLinkRight title={"Recruitment Information"} logo={"https://www.police.gov.bd/storage/upload/announcement/police-info_file844111724.png"}/>
-{/* 3 */}
-<ImportantLinkRight title={"Procurement/Tender Notice"} logo={"https://www.police.gov.bd/storage/upload/announcement/police-info_file900111750.png"}/>
-{/* 4 */}
-<ImportantLinkRight title={"Legal Instrument"} logo={"https://www.police.gov.bd/storage/upload/announcement/police-info_file416111811.png"}/>
-{/* 5 */}
-<ImportantLinkRight title={"Detective Magazine"} logo={"https://www.police.gov.bd/storage/upload/announcement/nPpcA1nh6Z1E5qHjCLChlp4TaUqtRvyiDGqefxCL.png"}/>
-{/* 6 */}
-<ImportantLinkRight title={"Diabetis-Covid19"} logo={"https://www.police.gov.bd/storage/upload/announcement/6gy49XKLLc9NW3VOqBo65Q7QWmLdrANgGQwsIgKw.png"}/>
-{/* 7 */}
-<ImportantLinkRight title={"Exam Result"} logo={"https://www.police.gov.bd/storage/upload/announcement/WUUH1aE25QKwJpM25INyfTw0WbUoz8MBxjjgyv1Q.jpeg"}/>
-{/* 8 */}
-<ImportantLinkRight title={"Accident Info"} logo={"https://www.police.gov.bd/storage/upload/announcement/OPMKYwuFD9fKrGESoCgpWC5yX1Tfmco1fECypo1e.jpeg"}/>
-</div>)
+        <ImportantLinkRight title={"Press Release"} logo={press} />
+        {/* 2 */}
+        <ImportantLinkRight
+          title={"Recruitment Information"}
+          logo={Recruitment}
+        />
+        {/* 3 */}
+        <ImportantLinkRight
+          title={"Procurement/Tender Notice"}
+          logo={procurement}
+        />
+        {/* 4 */}
+        <ImportantLinkRight title={"Legal Instrument"} logo={legal} />
+        {/* 5 */}
+        <ImportantLinkRight title={"Detective Magazine"} logo={detective} />
+        {/* 6 */}
+        <ImportantLinkRight title={"Diabetis-Covid19"} logo={diabetis} />
+        {/* 7 */}
+        <ImportantLinkRight title={"Exam Result"} logo={exsm} />
+        {/* 8 */}
+        <ImportantLinkRight title={"Accident Info"} logo={Accident} />
+      </div>
+    );
 }
 
 export default RightContentArea;
